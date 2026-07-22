c428dbbcdba1a081d6750e7886fc95d324046556
CarXMarketWorld <muhammedhastas8@gmail.com>
2026-07-22 05:30:19 +0300

    Add menu toggle functionality to navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+const menu = document.getElementById("menu-toggle");
+const nav = document.getElementById("navbar");
 
+menu.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
