fb2be0249be06b114aa6efed647edd79239fc6ef
CarXMarketWorld <muhammedhastas8@gmail.com>
2026-07-22 14:02:15 +0300

    Remove addAccount function and add debug alerts
    Removed the addAccount function and added alerts for debugging.

--- js/admin.js
+++ js/admin.js
@@ -1,43 +1,10 @@
-alert("admin.js yüklendi");
+alert("1");
+
 const supabaseUrl = "https://zyznqwyveslkxjxpzhpi.supabase.co";
 const supabaseKey = "sb_publishable_jlj-NyBWyFkseo57_TtVxA_WRVFO2RK";
 
+alert("2");
+
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-async function addAccount() {
-
-    const title = document.getElementById("title").value;
-    const cars = document.getElementById("cars").value;
-    const money = document.getElementById("money").value;
-    const level = document.getElementById("level").value;
-    const price = document.getElementById("price").value;
-    const image = document.getElementById("image").value;
-    const telegram = document.getElementById("telegram").value;
-
-    const { error } = await supabase
-        .from("accounts")
-        .insert([{
-            title,
-            cars,
-            money,
-            level,
-            price,
-            image,
-            telegram
-        }]);
-
-    if (error) {
-        alert(error.message);
-        return;
-    }
-
-    alert("Hesap başarıyla eklendi.");
-
-    document.getElementById("title").value = "";
-    document.getElementById("cars").value = "";
-    document.getElementById("money").value = "";
-    document.getElementById("level").value = "";
-    document.getElementById("price").value = "";
-    document.getElementById("image").value = "";
-    document.getElementById("telegram").value = "";
-}
+alert("3");
