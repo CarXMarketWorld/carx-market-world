2569d689eb993d5ecf7e62abffed034799b9c859
CarXMarketWorld <muhammedhastas8@gmail.com>
2026-07-22 05:40:02 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ const nav = document.getElementById("navbar");
 menu.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
+
+document.querySelectorAll("#navbar a").forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
