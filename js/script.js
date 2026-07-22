cfe6e0a7ca4612ea426df2f9b3baece39ebcc43f
CarXMarketWorld <muhammedhastas8@gmail.com>
2026-07-22 11:30:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
 const supabaseUrl = "https://zyznqwyveslkxjxpzhpi.supabase.co";
 const supabaseKey = "sb_publishable_jlj-NyBWyFkseo57_TtVxA_WRVFO2RK";
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-const menu = document.getElementById("menu-toggle");
-const nav = document.getElementById("navbar");
 
-menu.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});
-
-document.querySelectorAll("#navbar a").forEach(link => {
-    link.addEventListener("click", () => {
-        nav.classList.remove("active");
-    });
-});
 async function loadAccounts() {
+
   const { data, error } = await supabase
     .from("accounts")
     .select("*");
 
-  console.log(data, error);
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const container = document.getElementById("accounts-list");
+  container.innerHTML = "";
+
+  data.forEach(account => {
+    container.innerHTML += `
+      <div class="card">
+        <img src="${account.image}" alt="${account.title}">
+        <h3>${account.title}</h3>
+        <p>🚗 Araç: ${account.cars}</p>
+        <p>💰 Para: ${account.money}</p>
+        <p>⭐ Seviye: ${account.level}</p>
+        <p>💵 Fiyat: ${account.price}</p>
+        <a href="${account.telegram}" target="_blank">Telegram</a>
+      </div>
+    `;
+  });
 }
 
 loadAccounts();
